9182c10a314d21d118964c4a412fae0fe8ee7b2c
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-13 16:27:51 -0500

    changes: deleted stack importation from layout on app route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line react/react-in-jsx-scope
 
 import './global.css';
-import {SplashScreen, Stack} from 'expo-router';
+import {SplashScreen, /* Stack, */ Slot} from 'expo-router';
 import {useFonts} from 'expo-font';
 import {useEffect} from 'react';
 
@@ -23,7 +23,7 @@ const RootLayout = () => {
   }, [fontsLoaded, error]);
   if (!fontsLoaded && !error) return null;
 
-  return <Stack />;
+  return <Slot />;
 };
 
 export default RootLayout;
