6ee8873a023ef27844d8da35c41447795d2bc6a8
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-13 16:35:02 -0500

    changes: add on stack folder layout stack screen tags  to label the stacks

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -2,7 +2,34 @@
 import {Stack} from 'expo-router';
 
 const StackLayout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="home/index"
+        options={{
+          title: 'Home Screen',
+        }}
+      />
+      <Stack.Screen
+        name="products/index"
+        options={{
+          title: 'Productos Screen',
+        }}
+      />
+      <Stack.Screen
+        name="profile/index"
+        options={{
+          title: 'Profile Screen',
+        }}
+      />
+      <Stack.Screen
+        name="settings/index"
+        options={{
+          title: 'Settings',
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default StackLayout;
