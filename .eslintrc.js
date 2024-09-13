f5d34a40de57893b463b925d995c18ae368e4a79
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-13 05:17:05 -0500

    changes: prettier eslint fconfig, deleted initial folder

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+export default {
+  root: true,
+  extends: [
+    'expo',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+  "semi": true,
+  "trailingComma": "all",
+  "singleQuote": true,
+  "printWidth": 80,
+  "tabWidth": 2,
+  "endOfLine": "auto",
+  "arrowParens": "avoid", 
+  "bracketSameLine": true, 
+  "bracketSpacing": false 
+};
