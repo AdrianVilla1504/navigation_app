dfe59e620860a5aeab3e7004a855eff9fc07dd1b
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-16 15:09:31 -0500

    changes: add drawer screen options and icon color and size for drawer screen

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,22 +1,38 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import {Ionicons} from '@expo/vector-icons';
 import {Drawer} from 'expo-router/drawer';
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
-      <Drawer.Screen
-        name="schedule/index" // This is the name of the page and must match the url from root
-        options={{
-          drawerLabel: 'Schedule',
-          title: 'Horario',
-        }}
-      />
+    <Drawer
+        
+      screenOptions={{
+        overlayColor: 'rgba(0,0,0,0,4)',
+        drawerActiveTintColor: 'indigo',
+        headerShadowVisible: false,
+        sceneContainerStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
       <Drawer.Screen
         name="user/index" // This is the name of the page and must match the url from root
         options={{
           drawerLabel: 'User',
           title: 'Usuario',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="schedule/index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: 'Schedule',
+          title: 'Horario',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
         }}
       />
     </Drawer>
