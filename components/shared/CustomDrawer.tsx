7a830d59a23685678eaa2ee54e40580488361f40
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-16 15:31:49 -0500

    changes: added drawer content component props and drawer item list to custom drawer

--- components/shared/CustomDrawer.tsx
+++ components/shared/CustomDrawer.tsx
@@ -1,15 +1,20 @@
 import {Text, View} from 'react-native';
 import React from 'react';
-import {DrawerContentScrollView} from '@react-navigation/drawer';
+import {DrawerContentComponentProps} from '@react-navigation/drawer/lib/typescript/src/types';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 
-const CustomDrawer = () => {
+const CustomDrawer = (props: DrawerContentComponentProps) => {
   return (
-    <DrawerContentScrollView>
+    <DrawerContentScrollView {...props}>
       <View className="flex justify-center items-center mx-3 p-10 mb-10 h-[150px] rounded-xl bg-primary">
         <View className="flex justify-center items-center bg-white rounded-full h-24 w-24">
           <Text className="text-primary font-work-black text-3xl">FH</Text>
         </View>
       </View>
+      <DrawerItemList {...props} />
     </DrawerContentScrollView>
   );
 };
